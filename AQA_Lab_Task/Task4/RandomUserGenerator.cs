c098cf5e1fd4b5e5ca3f596cef73929ecda8c603
Dima Layeuski <dima.layeuski@gmail.com>
2022-03-18 00:07:31 +0300

    Task-4: Correction of grammatical mistake

--- AQA_Lab_Task/Task4/RandomUserGenerator.cs
+++ AQA_Lab_Task/Task4/RandomUserGenerator.cs
@@ -9,12 +9,12 @@ public class RandomUserGenerator
     public IEnumerable<IPrintable> GenerateRandomUsers()
     {
         var usersCollection = new List<IPrintable?>();
-        AddToColection(usersCollection, _employeeCount, UserType.Employee);
-        AddToColection(usersCollection, _candidateCount, UserType.Candidate);
+        AddToCollection(usersCollection, _employeeCount, UserType.Employee);
+        AddToCollection(usersCollection, _candidateCount, UserType.Candidate);
         return usersCollection!;
     }
 
-    private void AddToColection(List<IPrintable?> collection, int count, UserType userType)
+    private void AddToCollection(List<IPrintable?> collection, int count, UserType userType)
     {
         for (var i = 0; i < count; i++)
         {
