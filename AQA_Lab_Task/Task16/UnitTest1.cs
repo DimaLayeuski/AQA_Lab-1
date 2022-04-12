6a4fa3a8988131b9fb2acd3f982a39f401c6fed2
Dima Layeuski <dima.layeuski@gmail.com>
2022-04-13 01:36:46 +0300

    TASK-16: Add .gitignore and *.sln file, change README.md

---
+++ AQA_Lab_Task/Task16/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Task16;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
